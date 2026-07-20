3f3e63171676939436d0aa4f568410d8a3a7585b
Pawaris <pawaris.sabuathong@gmail.com>
2026-07-20 19:30:05 +0700

    ex04

---
+++ ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void    ft_ultimate_div_mod(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = temp / *b;
+    *b = temp % *b;
+}
+
+int main()
+{
+    int a = 10;
+    int b = 3;
+
+    ft_ultimate_div_mod(&a , &b);
+    printf("%d\n", a);
+    printf("%d\n", b);
+}
