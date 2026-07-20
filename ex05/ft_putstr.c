65961a4e36b2bcfb32cb46c19f94d40654051fbf
Pawaris <pawaris.sabuathong@gmail.com>
2026-07-20 20:34:16 +0700

    ex05

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void    ft_putstr(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+
+int main(void)
+{
+    ft_putstr("Hello, World");
+    write(1, "\n", 1);  //newline 
+    ft_putstr("WASSAP");
+    write(1, "\n", 1);  //newline
+    ft_putstr("Broooo");
+}
