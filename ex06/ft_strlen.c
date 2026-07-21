03d4ff8a6e3c34b2a7c0fa8d4acc794aac8609b0
Pawaris <pawaris.sabuathong@gmail.com>
2026-07-21 19:06:38 +0700

    ex06_change_variable_name

--- ex06/ft_strlen.c
+++ ex06/ft_strlen.c
@@ -14,12 +14,12 @@ int main()
 {
     int count;
     int count1;
-    char str[] = "Hello";//5 characters
-    char a[] = "Wassappppppppppppppppppppppppp";//30 characters
+    char a[] = "Hello";//5 characters
+    char b[] = "Wassappppppppppppppppppppppppp";//30 characters
 
 
-    count = ft_strlen(&str[0]);
-    count1 = ft_strlen(&a[0]);
+    count = ft_strlen(&a[0]);
+    count1 = ft_strlen(&b[0]);
     printf("%d\n", count);
     printf("%d\n", count1);
 }
